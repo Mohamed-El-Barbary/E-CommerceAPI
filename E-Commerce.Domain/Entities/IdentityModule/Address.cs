dce3e2c87f0eb8ec638b03444ce38959179dcb6e
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-01-08 02:32:30 +0200

    refactor(IdentityModule): update address entity & add migrations

--- E-Commerce.Domain/Entities/IdentityModule/Address.cs
+++ E-Commerce.Domain/Entities/IdentityModule/Address.cs
@@ -5,7 +5,7 @@ public class Address
     public int Id { get; set; }
     public string City { get; set; } = default!;
     public string Street { get; set; } = default!;
-    public string County { get; set; } = default!;
+    public string Country { get; set; } = default!;
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
     public ApplicationUser User { get; set; } = default!;
