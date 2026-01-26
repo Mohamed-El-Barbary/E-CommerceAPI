ffbb7ec7ff79d029ba376c582598efd67cfa0fe4
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-01-26 19:29:16 +0200

    refactor(payment-module): restructure basket and order entities

--- E-Commerce.Domain/Entities/OrderModule/Order.cs
+++ E-Commerce.Domain/Entities/OrderModule/Order.cs
@@ -6,6 +6,7 @@ public class Order : BaseEntity<Guid>
     public string Phone { get; set; } = default!;
     public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
     public OrderStatus Status { get; set; } = OrderStatus.Pending;
+    public string PaymentIntentId { get; set; } = default!;
     public OrderAddress Address { get; set; } = default!;
     public DeliveryMethod DeliveryMethod { get; set; } = default!;
     public int  DeliveryMethodId { get; set; }
